b005f7847b023023a909f89640edfe5b974c1486
WOONBE <kepull2918@naver.com>
2024-09-04 10:09:06 +0900

    feat(#S11P21D106-135) : 백엔드 기초 환경 세팅

---
+++ Arti-Backend/src/main/java/com/d106/arti/ArtiApplication.java
@@ -0,0 +1,13 @@
+package com.d106.arti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtiApplication.class, args);
+	}
+
+}
