5aa8195fa12f14ac50c08785f126a62edd541003
upsk1 <55921031+upsk1@users.noreply.github.com>
2024-09-09 13:42:42 +0900

    feat: 배너 클릭시 이동 구현

---
+++ Android/ArtI/app/src/main/java/com/hexa/arti/ui/search/ArtBannerFragment.kt
@@ -0,0 +1,17 @@
+package com.hexa.arti.ui.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hexa.arti.R
+import com.hexa.arti.config.BaseFragment
+import com.hexa.arti.databinding.FragmentArtBannerBinding
+
+
+class ArtBannerFragment : BaseFragment<FragmentArtBannerBinding>(R.layout.fragment_art_banner) {
+    override fun init() {
+
+    }
+}
