bd0b9ecb626a01a249504c11820f42850539c476
mdj928 <moondj928@gmail.com>
2024-09-10 10:09:23 +0900

    feat: 프로필 편집 UI, 비밀번호 수정 UI 개발

--- Android/ArtI/app/src/main/java/com/hexa/arti/ui/profile/PortfolioFragment.kt
+++ Android/ArtI/app/src/main/java/com/hexa/arti/ui/profile/PortfolioFragment.kt
@@ -1,21 +1,32 @@
 package com.hexa.arti.ui.profile
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import com.hexa.arti.R
 import com.hexa.arti.config.BaseFragment
 import com.hexa.arti.databinding.FragmentPortfolioBinding
+import com.hexa.arti.ui.MainActivity
 import com.hexa.arti.util.navigate
 
 class PortfolioFragment : BaseFragment<FragmentPortfolioBinding>(R.layout.fragment_portfolio) {
+    private lateinit var mainActivity : MainActivity
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
     override fun init() {
-
     }
 
+    override fun onResume() {
+        super.onResume()
+        mainActivity.hideBottomNav(false)
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.mypageBtn.setOnClickListener {
             navigate(R.id.action_portfolioFragment_to_myPageFragment)
         }
     }
+
 }
--- Android/ArtI/app/src/main/java/com/hexa/arti/ui/profile/ProfileDetailFragment.kt
+++ Android/ArtI/app/src/main/java/com/hexa/arti/ui/profile/ProfileDetailFragment.kt
@@ -1,13 +1,60 @@
 package com.hexa.arti.ui.profile
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.NavArgs
+import androidx.navigation.fragment.navArgs
 import com.hexa.arti.R
+import com.hexa.arti.config.BaseFragment
+import com.hexa.arti.databinding.FragmentProfileDetailBinding
+import com.hexa.arti.ui.MainActivity
+import com.hexa.arti.util.popBackStack
 
 
-class ProfileDetailFragment : Fragment() {
+class ProfileDetailFragment : BaseFragment<FragmentProfileDetailBinding>(R.layout.fragment_profile_detail) {
+    private lateinit var mainActivity: MainActivity
+    private val ars : ProfileDetailFragmentArgs by navArgs()
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+    override fun init() {
+    }
 
+    override fun onResume() {
+        super.onResume()
+        mainActivity.hideBottomNav(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+        initEvent()
+    }
+    private fun initView(){
+        with(binding){
+            when(ars.detailType){
+                0-> {
+                    profileDetailModifyCl.visibility = View.VISIBLE
+                }
+                1->{
+                    profilePassModifyCl.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+    private fun initEvent(){
+        with(binding){
+            profileDetailBackBtn.setOnClickListener { popBackStack() }
+            profilePassBackBtn.setOnClickListener { popBackStack() }
+            profileDetailCancelBtn.setOnClickListener { popBackStack() }
+            profilePassCancelBtn.setOnClickListener { popBackStack() }
+
+            // 프로필 수정
+            profileDetailModifyBtn.setOnClickListener { popBackStack() }
+            // 비밀번호 수정
+            profilePassModifyBtn.setOnClickListener { popBackStack() }
+        }
+    }
 }
