c6ec8ecb2ff9fd130b44edec5a874f04d4c9918c
mdj928 <moondj928@gmail.com>
2024-09-10 13:45:17 +0900

    design: 회원가입 디자인 수정

--- Android/ArtI/app/src/main/java/com/hexa/arti/ui/signup/SignUpFragment.kt
+++ Android/ArtI/app/src/main/java/com/hexa/arti/ui/signup/SignUpFragment.kt
@@ -1,5 +1,6 @@
 package com.hexa.arti.ui.signup
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,27 +9,36 @@ import android.view.ViewGroup
 import com.hexa.arti.R
 import com.hexa.arti.config.BaseFragment
 import com.hexa.arti.databinding.FragmentSignUpBinding
+import com.hexa.arti.ui.LoginActivity
 import com.hexa.arti.util.navigate
 import com.hexa.arti.util.popBackStack
 
 class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sign_up) {
     override fun init() {
     }
+    private lateinit var loginActivity: LoginActivity
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        loginActivity = context as LoginActivity
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         with(binding){
             signCancelBtn.setOnClickListener {
-                popBackStack()
+                loginActivity.moveLogin()
             }
             signBtn.setOnClickListener {
-                navigate(R.id.action_signUpFragment_to_loginFragment)
+                loginActivity.moveLogin()
             }
             signEmailBtn.setOnClickListener {
                 signCertificationTv.visibility = View.VISIBLE
                 signCertificationEt.visibility = View.VISIBLE
                 signCertificationBtn.visibility = View.VISIBLE
+            }
+            signBackBtn.setOnClickListener {
+                loginActivity.moveLogin()
             }
         }
     }
