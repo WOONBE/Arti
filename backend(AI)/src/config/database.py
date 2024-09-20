fa060895ff5d2dc4915de841b894ebe19dd3e8de
황병현 <grant0808@naver.com>
2024-09-20 14:38:50 +0900

    🛠 FIX : ec2 image path
    Former-commit-id: 8d06c77ec5fd2e0b6cf17af592e6075c0fe2d143

--- backend(AI)/src/config/database.py
+++ backend(AI)/src/config/database.py
@@ -14,7 +14,11 @@ db = secrets["DB"]
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{db.get('user')}@stg-yswa-kr-practice-db-master:{db.get('password')}@{db.get('host')}:{db.get('port')}/{db.get('database')}?charset=utf8"
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_recycle=28000,  # 28000초마다 연결을 재활용
+    pool_pre_ping=True   # MySQL 연결 확인
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
