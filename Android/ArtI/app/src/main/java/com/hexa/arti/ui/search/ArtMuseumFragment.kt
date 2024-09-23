e809874d3082a79e555f8a11a7c0d1692afb5483
upsk1 <55921031+upsk1@users.noreply.github.com>
2024-09-23 15:32:18 +0900

    feat: 아이템뷰 클릭시 포커스 표시 및 작품이미지 표시

--- Android/ArtI/app/src/main/java/com/hexa/arti/ui/search/ArtMuseumFragment.kt
+++ Android/ArtI/app/src/main/java/com/hexa/arti/ui/search/ArtMuseumFragment.kt
@@ -1,6 +1,6 @@
 package com.hexa.arti.ui.search
 
-import android.util.Log
+import com.bumptech.glide.Glide
 import com.hexa.arti.R
 import com.hexa.arti.config.BaseFragment
 import com.hexa.arti.data.model.home.ArtTheme
@@ -8,11 +8,12 @@ import com.hexa.arti.data.model.search.PreviewImage
 import com.hexa.arti.databinding.FragmentArtMuseumBinding
 import com.hexa.arti.ui.home.adapter.ThemeAdapter
 import com.hexa.arti.ui.search.adapter.PreviewAdapter
+import kotlin.random.Random
 
 class ArtMuseumFragment : BaseFragment<FragmentArtMuseumBinding>(R.layout.fragment_art_museum) {
 
-    private val previewAdapter = PreviewAdapter {
-        Log.d("확인", "클릭 확인")
+    private val previewAdapter = PreviewAdapter { clickedImage ->
+        changeFocusItem(clickedImage)
     }
 
     private val themeAdapter = ThemeAdapter()
@@ -23,12 +24,26 @@ class ArtMuseumFragment : BaseFragment<FragmentArtMuseumBinding>(R.layout.fragme
 
     private fun initViews() {
 
+        val mockUrl = listOf(
+            "https://upload.wikimedia.org/wikipedia/commons/8/87/2012_Henrique_Matos_Optical_Motion_01.jpg",
+            "https://i0.wp.com/blog.rightbrain.co.kr/wp-content/uploads/2014/11/victorvasarely_02.jpg?fit=408%2C328&ssl=1",
+            "https://cdn.knupresscenter.com/news/photo/201103/14901_1621_4915.jpg",
+            "https://cdn.knupresscenter.com/news/photo/201103/14901_1622_4915.jpg",
+            "https://cicamuseum.com/wp-content/uploads/2014/03/semikim_01.jpg",
+        )
+
         binding.rvPreview.apply {
             adapter = previewAdapter
             val previewMockData = mutableListOf<PreviewImage>()
             for (i in 0..20) {
-                previewMockData.add(PreviewImage(id = i, url = ""))
+                previewMockData.add(PreviewImage(id = i, url = mockUrl[Random.nextInt(5)]))
             }
+            previewMockData[0] = previewMockData[0].copy(isFocus = true)
+            Glide.with(requireContext())
+                .load(previewMockData[0].url)
+                .error(R.drawable.gallery_example)
+                .into(binding.ivArtImage)
+
             previewAdapter.submitList(previewMockData)
         }
 
@@ -42,6 +57,21 @@ class ArtMuseumFragment : BaseFragment<FragmentArtMuseumBinding>(R.layout.fragme
 
             themeAdapter.submitList(artThemeList)
         }
+
+        
+    }
+
+    private fun changeFocusItem(clickedImage: PreviewImage) {
+        val updateList = previewAdapter.currentList.map { previewImage ->
+            previewImage.copy(isFocus = (previewImage.id == clickedImage.id))
+        }
+
+        Glide.with(requireContext())
+            .load(clickedImage.url)
+            .error(R.drawable.gallery_sample2)
+            .into(binding.ivArtImage)
+
+        previewAdapter.submitList(updateList)
     }
 
 }
