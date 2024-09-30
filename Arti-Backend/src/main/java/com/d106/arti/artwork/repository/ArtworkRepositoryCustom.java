cefac4220b13ae4fb25ea577486f6ec613636fff
박민희 <minuteman0212@naver.com>
2024-09-30 10:00:04 +0900

    Revert "Merge branch 'be/feat/log' into 'master'"

--- Arti-Backend/src/main/java/com/d106/arti/artwork/repository/ArtworkRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.d106.arti.artwork.repository;
-
-import com.d106.arti.artwork.domain.NormalArtWork;
-
-import java.util.List;
-
-public interface ArtworkRepositoryCustom {
-    List<NormalArtWork> search(String keyword);
-}
