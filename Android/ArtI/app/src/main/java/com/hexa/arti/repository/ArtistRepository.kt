1840dd326076af5850fa8d061dc12d2e5c614171
김민조 <upsk1@naver.com>
2024-10-02 02:13:55 +0900

    feat: 랜덤 화가 api 연결

--- Android/ArtI/app/src/main/java/com/hexa/arti/repository/ArtistRepository.kt
+++ Android/ArtI/app/src/main/java/com/hexa/arti/repository/ArtistRepository.kt
@@ -4,4 +4,6 @@ import com.hexa.arti.data.model.search.Artist
 
 interface ArtistRepository {
     suspend fun getArtist(keyword: String): Result<List<Artist>>
+
+    suspend fun getRandomArtists(): Result<List<Artist>>
 }
--- Android/ArtI/app/src/main/java/com/hexa/arti/ui/search/artist/ArtistBannerFragment.kt
+++ Android/ArtI/app/src/main/java/com/hexa/arti/ui/search/artist/ArtistBannerFragment.kt
@@ -1,39 +1,45 @@
 package com.hexa.arti.ui.search.artist
 
 import android.util.Log
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.hexa.arti.R
 import com.hexa.arti.config.BaseFragment
-import com.hexa.arti.data.model.search.Artist
 import com.hexa.arti.databinding.FragmentArtistBannerBinding
 import com.hexa.arti.ui.search.adapter.ArtistAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ArtistBannerFragment :
     BaseFragment<FragmentArtistBannerBinding>(R.layout.fragment_artist_banner) {
 
+    private val viewModel: ArtistBannerViewModel by viewModels()
 
     private val artistAdapter = ArtistAdapter {
         Log.d("확인", "아이템클릭 확인")
     }
 
     override fun init() {
+        initObserve()
         initViews()
+
+        viewModel.getRandomArtists()
     }
 
-    private fun initViews(){
+    private fun initObserve() {
+        viewModel.resultArtists.observe(viewLifecycleOwner) {
+            Log.d("확인", "결과는 ${it}")
+            if (it.isEmpty()) {
+                artistAdapter.submitList(emptyList())
+            } else {
+                artistAdapter.submitList(it)
+            }
+        }
+
+    }
+
+    private fun initViews() {
         binding.rvArtist.adapter = artistAdapter
-
-        val mockData = listOf(
-            Artist(artistId = 0, korName = "잠만보1", engName = "aa", imageUrl = "sad"),
-            Artist(artistId = 1, korName = "잠만보1", engName = "aa", imageUrl = "sad"),
-            Artist(artistId = 2, korName = "잠만보1", engName = "aa", imageUrl = "sad"),
-            Artist(artistId = 3, korName = "잠만보1", engName = "aa", imageUrl = "sad"),
-            Artist(artistId = 4, korName = "잠만보1", engName = "aa", imageUrl = "sad"),
-            Artist(artistId = 5, korName = "잠만보1", engName = "aa", imageUrl = "sad"),
-            Artist(artistId = 6, korName = "잠만보1", engName = "aa", imageUrl = "sad"),
-        )
-
-        artistAdapter.submitList(mockData)
 
         binding.ivBack.setOnClickListener {
             findNavController().popBackStack()
