02e1de5ee2239ca3be5591f97b4a48c7b5456ade
upsk1 <55921031+upsk1@users.noreply.github.com>
2024-10-04 10:14:19 +0900

    feat: 화가 클릭 이벤트 추가

--- Android/ArtI/app/src/main/java/com/hexa/arti/ui/search/artist/ArtistBannerViewModel.kt
+++ Android/ArtI/app/src/main/java/com/hexa/arti/ui/search/artist/ArtistBannerViewModel.kt
@@ -1,6 +1,5 @@
 package com.hexa.arti.ui.search.artist
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -18,13 +17,11 @@ class ArtistBannerViewModel @Inject constructor(
     private val _resultArtists = MutableLiveData<List<Artist>>()
     val resultArtists = _resultArtists
 
-    fun getRandomArtists(){
+    fun getRandomArtists() {
         viewModelScope.launch {
             artistRepository.getRandomArtists().onSuccess { response ->
                 _resultArtists.value = response
-                Log.d("확인","성공 ${response}")
             }.onFailure { error ->
-                Log.d("확인","실패 ${error}")
                 if (error is ApiException) {
                     _resultArtists.value = emptyList()
                 }
