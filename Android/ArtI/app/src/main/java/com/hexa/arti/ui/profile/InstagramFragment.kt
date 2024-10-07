8975c1fc0a2fdc7eb33b691f8adda652cc8a4ea0
upsk1 <55921031+upsk1@users.noreply.github.com>
2024-10-07 13:28:16 +0900

    feat: 인스타그램 url 추출

--- Android/ArtI/app/src/main/java/com/hexa/arti/ui/profile/InstagramFragment.kt
+++ Android/ArtI/app/src/main/java/com/hexa/arti/ui/profile/InstagramFragment.kt
@@ -1,5 +1,7 @@
 package com.hexa.arti.ui.profile
 
+import android.util.Log
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.hexa.arti.R
 import com.hexa.arti.config.BaseFragment
@@ -8,10 +10,27 @@ import com.hexa.arti.databinding.FragmentInstagramBinding
 class InstagramFragment : BaseFragment<FragmentInstagramBinding>(R.layout.fragment_instagram) {
 
     override fun init() {
+
+        binding.webView.settings.javaScriptEnabled = true
+
         binding.webView.apply {
-            webViewClient = WebViewClient()
-            settings.javaScriptEnabled = true
+            webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                    if (url != null && url.contains("callback?code")) {
+                        makeToast("인증 성공하였습니다.")
+                        Log.d("확인", "인증 성공 ${url}")
+                        binding.webView.loadUrl("javascript:window.Android.processHTML(document.documentElement.outerHTML);")
+                        return true
+                    }
+                    return super.shouldOverrideUrlLoading(view, url)
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+                }
+            }
             loadUrl("http://j11d106.p.ssafy.io/instagram/redirect")
         }
     }
+
 }
