66639e3114e2bd37100bb47f7b702e68dc254b7e
박민희 <minuteman0212@naver.com>
2024-10-08 11:12:33 +0900

    fix: logback-spring.xml 에서 logstash 경로 설정

--- Arti-Backend/src/main/java/com/d106/arti/global/interceptor/ImageInterceptor.java
+++ Arti-Backend/src/main/java/com/d106/arti/global/interceptor/ImageInterceptor.java
@@ -2,25 +2,24 @@ package com.d106.arti.global.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Slf4j
 public class ImageInterceptor implements HandlerInterceptor {
 
-    public static final String LOG_ID = "logId";
-
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
         Object handler) throws Exception {
-
-        String requestURI = request.getRequestURI();
-
-        String uuid = UUID.randomUUID().toString();
-        request.setAttribute(LOG_ID, uuid);
-
-        log.info("REQUEST  [{}][{}][{}]", uuid, requestURI, handler);
+        MDC.put("requestURI", request.getRequestURI());
+        log.info("IMAGE");
         return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+        Object handler, Exception ex) throws Exception {
+        MDC.clear();
     }
 }
