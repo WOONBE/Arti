c7f1b16fe1f37e3831f69a0d162256ef27f7a538
박민희 <minuteman0212@naver.com>
2024-10-10 12:11:43 +0900

    feat: 로그인, 회원가입 입력값 검증 로직 구현

--- Arti-Backend/src/main/java/com/d106/arti/auth/AuthenticationController.java
+++ Arti-Backend/src/main/java/com/d106/arti/auth/AuthenticationController.java
@@ -2,14 +2,14 @@ package com.d106.arti.auth;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/auth")
@@ -20,22 +20,22 @@ public class AuthenticationController {
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody RegisterRequest request
+        @Validated @RequestBody RegisterRequest request
     ) {
         return ResponseEntity.ok(service.register(request));
     }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
-            @RequestBody AuthenticationRequest request
+        @Validated @RequestBody AuthenticationRequest request
     ) {
         return ResponseEntity.ok(service.authenticate(request));
     }
 
     @PostMapping("/refresh-token")
     public void refreshToken(
-            HttpServletRequest request,
-            HttpServletResponse response
+        HttpServletRequest request,
+        HttpServletResponse response
     ) throws IOException {
         service.refreshToken(request, response);
     }
--- Arti-Backend/src/main/java/com/d106/arti/auth/RegisterRequest.java
+++ Arti-Backend/src/main/java/com/d106/arti/auth/RegisterRequest.java
@@ -1,6 +1,7 @@
 package com.d106.arti.auth;
 
-import com.d106.arti.member.domain.Role;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +13,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+    @NotEmpty(message = "이메일은 필수 입력값입니다.")
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
     private String email;
+
+    @NotEmpty(message = "비밀번호는 필수 입력값입니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
+
     private String nickname;
 }
